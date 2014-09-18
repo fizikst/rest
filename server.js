6eb0346e6088dd1bebc5ce685eea310a9dff3357
Stas <s.shilyaev@original.ru>
2014-09-18 16:11:35 +0400

    first commit

---
+++ server.js
@@ -0,0 +1,114 @@
+// Module dependencies.
+var application_root = __dirname,
+    express = require( 'express' ); //Web framework
+    path = require( 'path' ),           //Utilities for dealing with file paths
+    mongoose = require( 'mongoose' );   //Used for accessing a MongoDB database
+
+//Connect to database
+mongoose.connect( 'mongodb://localhost/discount' );
+
+//Create server
+var app = express();
+
+//Schema
+var DiscountSchema = new mongoose.Schema({
+    title: String,
+    price: String
+});
+//Model
+var DiscountModel = mongoose.model( 'Discount', DiscountSchema );
+
+// Configure server
+app.configure( function() {
+    //parses request body and populates request.body
+    app.use( express.bodyParser() );
+
+    //checks request.body for HTTP method overrides
+    app.use( express.methodOverride() );
+
+    //perform route lookup based on url and HTTP method
+    app.use( app.router );
+
+    //Show all errors in development
+    app.use( express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+//Router
+//Get a list of all books
+app.get( '/api/v1/discounts', function( request, response ) {
+    return DiscountModel.find(function( err, discounts ) {
+        if( !err ) {
+            return response.send({'discounts':discounts});
+        } else {
+            console.log( err );
+            return response.send('ERROR');
+        }
+    });
+});
+//Insert a new book
+app.post( '/api/v1/discounts', function( request, response ) {
+	console.log(request);
+    var discount = new DiscountModel({
+        title: request.body.title,
+        price: request.body.price
+    });
+    console.log(request.body.title);
+    discount.save( function( err ) {
+        if( !err ) {
+            console.log( 'created' );
+            return response.send( discount );
+        } else {
+            console.log( err );
+            return response.send('ERROR');
+        }
+    });
+});
+//Get a single book by id
+app.get( '/api/v1/discounts/:id', function( request, response ) {
+    return DiscountModel.findById( request.params.id, function( err, discount ) {
+        if( !err ) {
+            return response.send( discount );
+        } else {
+            console.log( err );
+            return response.send('ERROR');
+        }
+    });
+});
+//Update a book
+app.put( '/api/v1/discounts/:id', function( request, response ) {
+    return DiscountModel.findById( request.params.id, function( err, discount ) {
+        discount.title = request.body.title;
+        discount.price = request.body.price;
+
+        return discount.save( function( err ) {
+            if( !err ) {
+                console.log( 'discount updated' );
+                return response.send( discount );
+            } else {
+                console.log( err );
+                return response.send('ERROR');
+            }
+        });
+    });
+});
+
+//Delete a book
+app.delete( '/api/v1/discounts/:id', function( request, response ) {
+    DiscountModel.findById( request.params.id, function( err, discount ) {
+        return discount.remove( function( err ) {
+            if( !err ) {
+                console.log( 'Discount removed' );
+                return response.send( '' );
+            } else {
+                console.log( err );
+                return response.send('ERROR');
+            }
+        });
+    });
+});
+
+//Start server
+var port = 4711;
+app.listen( port, function() {
+    console.log( 'Express server listening on port %d in %s mode', port, app.settings.env );
+});
